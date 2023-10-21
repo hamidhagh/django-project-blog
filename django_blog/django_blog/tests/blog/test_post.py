dae222cf55f1027519b4df97a15c13420cd29430
hamidhagh <hamidhaghverdi2002@gmail.com>
2023-10-21 17:28:48 +0200

    nc dev-1.0.4

--- django_blog/django_blog/tests/blog/test_post.py
+++ django_blog/django_blog/tests/blog/test_post.py
@@ -5,7 +5,7 @@ pytestmark = pytest.mark.django_db
 
 
 class TestPostSingle:
-    def test_post_single_url(self, client, post_factory):
+    def test_single_post_url(self, client, post_factory):
         post = post_factory()
         url = reverse("single-post", kwargs={"post": post.slug})
         response = client.get(url)
